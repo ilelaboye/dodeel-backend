dd2127ec4804f5d4a8038190fe651f4c538d896a
Mini_Rogue <johnabrahamtosin@gmail.com>
2023-09-27 16:00:24 +0100

    user report

--- routes/endpoints/user_report.js
+++ routes/endpoints/user_report.js
@@ -8,24 +8,37 @@ const { verifyToken } = tokenCallback()
 let routes = (app) => {
     app.post('/user_report', async (req, res) => {
         try {
-            
-            let user_report = new Report(req.body);
-            await user_report.save()
-            res.json(user_report)
+
+            const responses = verifyToken({ authToken: req.header('authorization') });
+            let user_report = new Report({ ...req.body, user_id: responses.data.id  });
+            await user_report.save();
+            res.json(user_report);
         }
         catch (err) {
             res.status(500).send(err)
         }
     });
 
-  
+
     // get all user_reports
-    app.get('/user_reports', async (req, res) => {
+    app.get('/admin/user_reports', async (req, res) => {
         try {
 
             let user_reports = await Report.find().sort({ createdAt: -1 })
-                .populate("report_id")
-               
+                .populate("report_id").populate("users")
+
+            res.json(user_reports)
+        }
+        catch (err) {
+            res.status(400).send(err)
+        }
+    });
+
+    app.get('/user/user_reports', async (req, res) => {
+        try {
+            const responses = verifyToken({ authToken: req.header('authorization') });
+            let user_reports = await Report.find({user_id: responses.data.id  }).sort({ createdAt: -1 })
+                .populate("report_id").populate("users")
             res.json(user_reports)
         }
         catch (err) {
